fa71aed5feb502cfabe4ec82b94277c870d10907
Vuducbao913 <vuducbao913@gmail.com>
2026-04-23 07:40:34 +0000

    update file train

--- train.sh
+++ train.sh
@@ -2,14 +2,14 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 # export CUDA_VISIBLE_DEVICES=0
 torchrun --nproc_per_node=8 training.py \
     --model-id "nguyenvulebinh/parakeet-avsr" \
-    --train_files /mnt/data/workspace/baovd5/asr-omni-main/data/train.jsonl,/mnt/data/workspace/baovd5/asr-omni-main/data/avsr_en_vox.jsonl \
-    --eval_files /mnt/data/workspace/baovd5/asr-omni-main/data/test.jsonl \
+    --train_files data/train.jsonl,data/avsr_en_vox.jsonl \
+    --eval_files data/test.jsonl \
     --batch-size 8 \
     --model-id nguyenvulebinh/parakeet-avsr \
     --output-dir model-bin/test \
     --freeze-vis-feat 1 \
     --learning-rate 2e-4 \
     --custom-tokenizer 0 \
-    --custom-tokenizer-path /mnt/data/workspace/baovd5/speech/parakeet-avsr/vocab/merged_unigram.model \
+    --custom-tokenizer-path vocab/vi_en_vocab.model \
     --save-steps 5000 \
     --eval-steps 5000 \
